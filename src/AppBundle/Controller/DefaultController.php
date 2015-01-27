d786a2e6e53c50a5247bf04c2405b621773e8f5a
bhdm <bhd.m@ya.ru>
2015-01-27 19:00:45 +0300

    add pages

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Client;
+use AppBundle\Form\ClientType;
+
+/**
+ * @package AppBundle\Controller
+ * @Route("/")
+ */
+class DefaultController extends Controller{
+
+    /**
+     * @Route("/page/{url}", name="page")
+     * @Template()
+     */
+    public function pageAction($url){
+        $page = $this->getDoctrine()->getRepository('AppBundle:Page')->findOneByUrl($url);
+        return array('page' => $page);
+    }
+}
